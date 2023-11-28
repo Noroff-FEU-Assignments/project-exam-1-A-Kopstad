cc3bc964a416b059d8bc71bed1e43f7902a922e1
A-Kopstad <126067888+A-Kopstad@users.noreply.github.com>
2023-11-28 14:16:07 +0100

    happy with hamburger

--- JavaScript/global.js
+++ JavaScript/global.js
@@ -150,18 +150,22 @@ document.addEventListener("DOMContentLoaded", function () {
     //Hamburger menu
 
     //Creating variables
-      const hamburgerMenu = document.querySelector('.burger-menu');
-      const navigation = document.querySelector ('.navigation');
-      
-      // A function opening the hamburger menu
-      hamburgerMenu.addEventListener('click', function () {
-        navigation.classList.toggle('active');
-      });
+    const hamburgerMenu = document.querySelector('.burger-menu');
+    const list = document.querySelector ('.navigation');
+    
+    // A function opening the hamburger menu
+    hamburgerMenu.addEventListener('click', function () {
+      list.classList.toggle('active');
+      hamburgerMenu.classList.toggle('active');
+    });
 
 
-      // A function closing the hamburger menu 
-      document.addEventListener ('click', function(event){
-        if (!hamburgerMenu.contains(event.target) && !navigation.contains(event.target) && navigation.classList.contains('active')){
-          navigation.classList.remove('active')
-        }
-      });
+    // A function closing the hamburger menu 
+    document.addEventListener ('click', function(event){
+      if (!hamburgerMenu.contains(event.target) && !list.contains(event.target) && list.classList.contains('active')){
+        list.classList.remove('active')
+      }
+    });
+
+
+
